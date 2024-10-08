7e8ceaf232f4b6c9c81132a3dbbd2efe5b3bea5f
Sol <sol6801@gmail.com>
2024-10-08 17:43:11 -0300

    Update seeds

--- mealSeed.js
+++ mealSeed.js
@@ -6,175 +6,175 @@ const main = async () => {
   const meals  = [
   {
     name: "Asado con ensalada",
-    urlImage: "https://cdn.shopify.com/s/files/1/0517/8825/1314/files/Support_1-Como_hacer_una_ensalada_perfecta_para_acompanar_tu_carne_asada.jpg?v=1654644518",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/asado.webp",
   },
   {
-    name: "Empanadas con chimichurri",
-    urlImage: "https://www.eatingwell.com/thmb/N7ZXshJJbijJrHCC2qebhFcaN7I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/air-fryer-empanadas-2000-44ba7e44e9734e52a807ba40624beb0d.jpg",
+    name: "Empanadas",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/empanadas.webp",
   },
   {
     name: "Milanesa con puré de papas",
-    urlImage: "https://www.indega.com.py/primicia/wp-content/uploads/2022/04/pure-de-papa-con-pollo-broaster-large-qlJiPE4lyS.jpeg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/milanesaconpure.webp",
   },
   {
     name: "Locro con pan casero",
-    urlImage: "https://www.essen.com.ar/contenido/objetos/1/Locro00053Marsala1.png",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/locroconpancasero.webp",
   },
   {
     name: "Provoleta con ensalada",
-    urlImage: "https://elferroviarioparrilla.com/images/prodctos_fijos/provoleta_1.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/provoleta_1.webp",
   },
   {
     name: "Bife de chorizo con papas fritas",
-    urlImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh9iyvUyMbsm6Apb0WES8aXWMAYVQjwRMbww&s",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/bifedechorizoyfritas.webp",
   },
   {
     name: "Ravioles con salsa de tomate",
-    urlImage: "https://okdiario.com/img/2014/07/04/raviolis-de-tomate-a-las-finas-hierbas-655x368.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/raviolesconfileto.webp",
   },
   {
     name: "Tarta de espinaca con ensalada verde",
-    urlImage: "https://i.pinimg.com/736x/2a/5b/f2/2a5bf24068644e48eb71984e5a18e6ee.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/tartadeespinacaconensalada.webp",
   },
   {
-    name: "Humita ",
-    urlImage: "https://locosxlaparrilla.com/wp-content/uploads/2015/02/Receta-recetas-locos-x-la-parrilla-locosxlaparrilla-receta-empanadas-humita-empanadas-humita-receta-empanadas-empanadas-3.jpg",
+    name: "Empanadas de Humita",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/empanadasdehumita.webp",
   },
   {
     name: "Milanesa a la napolitana con ensalada",
-    urlImage: "https://almacenvicenteabsurdo.com.ar/wp-content/uploads/2021/06/milas.png",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/milasconensalada.webp",
   },
   {
     name: "Pizza con fainá",
-    urlImage: "https://www.canal26.com/media/image/2024/03/08/592645.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/pizzaconfaina.webp",
   },
   {
     name: "Tortilla de papas con ensalada mixta",
-    urlImage: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiW8Zt2vG5vsVqLg3gaXArkjwQonA4E6uElmXE8iPnGIbCO-hmN7wky-mOTzk20IE2Wi09hSXA87t7kVv6Ow3fsrSgk8ESAI6zu48Ra5zjjXlUNcz8kJZloBdcklwYTFr6DxbwY397cVT8/s1600/Tortilla_calabaci%25CC%2581n_02.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/Tortillaconensalada.webp",
   },
   {
     name: "Ceviche con batatas fritas",
-    urlImage: "https://www.loveandlemons.com/wp-content/uploads/2020/04/ceviche.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/ceviche-con-batatas.webp",
   },
   {
     name: "Pollo al horno con papas",
-    urlImage: "https://resizer.glanacion.com/resizer/v2/pollo-al-horno-con-55UMVDSNXFEOFBW7XKPSAA3ZPE.jpg?auth=5199f7af2e5c05249b7d40a8096af95b6fd63b7da205a8553aeba1b07baa2772&width=768&height=512&quality=70&smart=true",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/pollo-al-horno.webp",
   },
   {
     name: "Cazuela con arroz",
-    urlImage: "https://i.blogs.es/ddbe80/arroz-en-cazuela-de-las-madre-de-los-roca/1366_2000.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/cazueladearrozconmariscos.webp",
   },
   {
     name: "Albondigas con arroz",
-    urlImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm1bfPD2MTJ_XEE5869A-fHQz-ALffbw0tOQ&s",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/albondigasconarroz.webp",
   },
   {
     name: "Pasta con salsa bolognesa ",
-    urlImage: "https://www.unileverfoodsolutions.com.co/dam/global-ufs/mcos/nola/colombia/calcmenu/recipes/CO-recipes/pasta-dishes/pasta-en-salsa-bolognesa/main-header.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/Arroz-con-pollo.webp",
   },
   {
     name: "Cordero al horno con papas al romero",
-    urlImage: "https://content-cocina.lecturas.com/medio/2023/05/10/paso_a_paso_para_hacer_pierna_de_cordero_al_horno_con_patatas_y_romero_resultado_final_d6dbcd56_1200x1200.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/corderoconpapas.webp",
   },
   {
     name: "Rollo de carne con puré de papas",
-    urlImage: "https://img.freepik.com/fotos-premium/rollo-cerdo-champinones-queso-servido-pure-papas-plato_286393-1510.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/rollo-carne.webp",
   },
   {
     name: "Salmón a la parrilla con vegetales asados",
-    urlImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRDPINqqYuFQB7KtxshNo7vaYnVVDa_dKwWQ&s",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/salmon.webp",
   },
   {
     name: "Pastel de pollo con verduras encurtidas",
-    urlImage: "https://content-cocina.lecturas.com/medio/2023/06/09/paso_a_paso_para_realizar_pastel_de_pollo_con_verduras_encurtidas_resultado_final_2a66cfc0_1200x1200.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/pasteldepollo.webp",
   },
   {
     name: "Estofado de carne con arroz",
-    urlImage: "https://www.gourmet.cl/wp-content/uploads/2016/09/estofado-de-carne-web-570x458.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/estofado-de-carne-web-570x458.webp",
   },
   {
-    name: "Tortilla de maíz con ensalada",
-    urlImage: "https://fotografias.antena3.com/clipping/cmsimages01/2022/03/31/9FD5DD45-F490-4A69-A986-C5BFF63FA809/tortillas-maiz_98.jpg?crop=8192,4609,x0,y852&width=1900&height=1069&optimize=high&format=webply",
+    name: "Tacos",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/tacos.webp",
   },
   {
     name: "Costillas a la parrilla con guarnición de ensalada",
-    urlImage: "https://media.istockphoto.com/id/1208591451/es/foto/costillas-de-espalda-de-beb%C3%A9-a-la-parrilla-con-ensalada-verde.jpg?s=612x612&w=0&k=20&c=C6DVq2tWs1MXyiVGTAvQfs_0aSFoKtR1aXUBe1Blwzc=",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/costillitas.webp",
   },
   {
-    name: "Pasta al horno con ensalada verde",
-    urlImage: "https://mandolina.co/wp-content/uploads/2024/05/Pasta-verduras.jpg",
+    name: "Pasta al horno verduras",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/Pasta-verduras.webp",
   },
   {
     name: "Pescado al horno con papas",
-    urlImage: "https://comedera.com/wp-content/uploads/sites/9/2022/02/pescado-al-horno-shutterstock_1919784281.jpg?w=4096",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/pescado-al-horno-shutterstock_1919784281.webp",
   },
   {
     name: "Camarones al ajillo con arroz",
-    urlImage: "https://www.noticiasdemexico.mx/u/fotografias/m/2024/3/20/f1280x720-55120_186795_5050.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/camaronesalajillo.webp",
   },
   {
     name: "Milanesa con ensalada",
-    urlImage: "https://www.feriamasticar.com.ar/wp-content/uploads/2024/04/milanesa-de-pollo-argentina-1.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/Arroz-con-pollo.webp",
   },
   {
     name: "Pasta primavera con verduras",
-    urlImage: "https://www.gastrolabweb.com/u/fotografias/m/2022/3/16/f638x638-27016_85183_5050.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/pasta.webp",
   },
   {
-    name: "Milanesa de pollo con ensalada ",
-    urlImage: "https://i.blogs.es/fda36a/como-hacer-milanesas-de-pollo-2-/1366_2000.jpg",
+    name: "Milanesa de pollo con ensalada",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/milaconensaladadepollo.webp",
   },
   {
-    name: "Ceviche con aguacate",
-    urlImage: "https://storage.googleapis.com/avena-recipes/2019/10/1571779990503.jpeg",
+    name: "Ceviche con Palta",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/cevicheconpalta.webp",
   },
   {
     name: "Pasta al pesto con pollo",
-    urlImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOgRTlRLpfjm7uv7UsSEWlg21c1-E25SLexA&s",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/pasataalpestoypollo.webp",
   },
   {
     name: "Papas rellenas con carne",
-    urlImage: "https://cdn7.kiwilimon.com/recetaimagen/26731/640x640/24093.jpg.webp",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/papasrellenas.webp",
   },
   {
-    name: "Sándwich de chorizo con ensalada",
-    urlImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP8NM4fRE2TJBMlkZ81kEyv2--UjJfHio3Sg&s",
+    name: "Choripán con ensalada",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/choripan.webp",
   },
   {
     name: "Sushi variado",
-    urlImage: "https://www.divinacocina.es/wp-content/uploads/2011/11/sushi-variado-bandeja.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/sushi-variado-bandeja.webp",
   },
   {
     name: "Pizza de muzzarella",
-    urlImage: "https://breaders.com.ar/web/wp-content/uploads/2022/01/pizzas-muzzarella.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/pizzas-muzzarella.webp",
   },
   {
     name: "Pasta con salsa de mariscos",
-    urlImage: "https://www.laylita.com/recetas/wp-content/uploads/Tallarin-con-mariscos.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/Tallarin-con-mariscos.webp",
   },
   {
     name: "Arroz con pollo",
-    urlImage: "https://www.aceitesdeolivadeespana.com/wp-content/uploads/2021/05/Arroz-con-pollo.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/Arroz-con-pollo.webp",
   },
   {
     name: "Calabaza rellena con carne",
-    urlImage: "https://canalcocina.es/medias/_cache/zoom-6d424acd1321fa08995f88c883bc199b-920-518.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/calabazarelleno.webp",
   },
   {
     name: "Ensalada César con pollo",
-    urlImage: "https://newmansown.com/wp-content/uploads/2022/03/Caesar-salad-with-croutons.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/cesar.webp",
   },
   {
     name: "Puchero",
-    urlImage: "https://www.recetasparaguay.com/base/stock/Recipe/puchero-paraguayo/puchero-paraguayo_web.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/puchero.webp",
   },
   {
     name: "Polenta con salsa bolognesa",
-    urlImage: "https://www.recetasnatura.com.ar/sites/default/files/655_x_475-_web_naturapolenta_con_salsa_bolonesa.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/polentaconbolognesa.webp",
   },
   {
     name: "Papas fritas con huevo y jamón",
-    urlImage: "https://canalcocina.es/medias/images/1404RecetasPor5EHuevosFritosPatatasChipJamon.jpg",
+    urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/mealsoptimizadas/papasconpancetayhuevo.webp",
   },
 ];
 for (const meal of meals) {
--- placeSeed.js
+++ placeSeed.js
@@ -4,195 +4,116 @@ const prisma = new PrismaClient();
 
 const main = async () => {
   const places = [
-    {
-      title: "Parque Tres de Febrero",
-urlImage: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/68/94/60/paseo-del-rosedal.jpg?w=1200&h=1200&s=1",
-    },
-    {
-      title: "Puerto Madero",
-      urlImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Puerto_Madero%2C_Buenos_Aires_%2840689219792%29.jpg/1200px-Puerto_Madero%2C_Buenos_Aires_%2840689219792%29.jpg",
-    },
-    {
-      title: "Caminito",
-      urlImage: "https://laguiadebuenosaires.com/wp-content/uploads/2018/06/caminito-800.jpg",
-    },
-    {
-      title: "Plaza de Mayo",
-      urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/piramide_de_mayo_flores_1200_1.jpg",
-    },
-    {
-      title: "Recoleta",
-      urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/floralis_generica_recoleta_dia_verde_1200_1.jpg",
-    },
-    {
-      title: "Jardín Botánico",
+      {
+          title: "Cine",
+          urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/cine.webp"
+      },
+      {
+          title: "Teatro",
+          urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/teatro.webp"
+      },
+      {
+          title: "Parque",
+          urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/parque.webp"
+      },
+      {
+          title: "Museo",
+          urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/museo.webp"
+      },
+      {
+        title: "Centro Comercial",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/centrocomercial.webp"
+      },
+      {
+        title: "Restaurante",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/restaurante.webp"
+      },
+      {
+        title: "Feria",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/feria.webp"
+      },
+      {
+        title: "Parque de Atracciones",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/parque-de-atracciones.webp"
+      },
+      {
+        title: "Zoológico",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/zoo.webp"
+      },
+      {
+        title: "Acuario",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/acuario.webp"
+      },
+      {
+        title: "Mirador",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/mirador.webp"
+      },
+      {
+        title: "Discoteca",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/discoteca.webp"
+      },
+      {
+        title: "Cafetería",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/cafeteria.webp"
+      },
+      {
+        title: "Cervecería",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/cerveceria.webp"
+      },
+      {
+          title: "Catedral",
+          urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/catedral.webp"
+      },
+      {
+        title: "Centro de Convenciones",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/convenciones.webp"
+      },
+      {
+        title: "Río",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/rio.webp"
+      },
+      {
+        title: "Playa",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/playa.webp"
+      },
+      {
+        title: "Jardín Botánico",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/jardinbotanico.webp"
+      },
+      {
+        title: "Parque Nacional",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/parquenacional.webp"
+      },
+      {
+        title: "Centro Recreativo",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/centrorecreativo.webp"
+      },
+      {
+        title: "Biblioteca",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/biblioteca.webp"
+      },
+      {
+        title: "Estadio",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/estadio.webp"
+      },
+      {
+        title: "Centro de Arte",
+        urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/centrodearte.webp"
+      },
+      {
+          title: "Campo de Golf",
+          urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/golf.webp"
+        },
+        {
+          title: "Galería de Arte",
+          urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/galeriadearte.webp"
+        },
+        {
+          title: "Complejo Deportivo",
+          urlImage: "https://plannerbyddy.s3.us-east-2.amazonaws.com/placesoptimizado/complejodeportivo.webp"
+      }
 
-      urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/botanico_jardin-1500x610.jpg",
-    },
-    {
-      title: "Avenida 9 de Julio",
-      urlImage: "https://www.serargentino.com/public/images/2020/10/16026013540-9-de-Julio-773x458.jpg",
-    },
-    {
-      title: "Palermo Soho",
-      urlImage: "https://res.cloudinary.com/hello-tickets/image/upload/c_limit,f_auto,q_auto,w_768/v1684536781/post_images/Argentina-170/Buenos-aires/Night/26004979950_dd6b9497ea_o_Cropped.jpg",
-    },
-    {
-      title: "Barrio Chino",
-      urlImage: "https://www.lanacion.com.ar/resizer/v2/barrio-chino-en-TPO7OBP6BFA5XP7JP35CN3ROCY.JPG?auth=dd00735b061dc5934450215eb9d7042cbdc6577ce5f66701e7a724c2ec62f9fc&width=420&height=280&quality=70&smart=true",
-    },
-    {
-      title: "Costanera Sur",
-      urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/costanera-sur-glorieta-1500x610.jpg",
-    },
-      {
-        title: "Teatro Colón",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/teatro_colon_panoramica_1200_1.jpg",
-      },
-      {
-        title: "Plaza San Martín",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/pza_san_martin_0.jpg",
-      },
-      {
-        title: "Palacio Barolo",
-        urlImage: "https://commons.wikimhttps://s7e6w6d2.rocketcdn.me/wp-content/uploads/2013/06/palacio%20barolo.jpgedia.org/wiki/Special:FilePath/Palacio_Barolo_2011.jpg",
-      },
-      {
-        title: "El Obelisco",
-        urlImage: "https://buenosaires.gob.ar/sites/default/files/media/image/2020/05/23/6a0c3268aa9051fd5646973dc070aa7edc1fcb8e.jpg",
-      },
-      {
-        title: "Museo Nacional de Bellas Artes",
-        urlImage: "https://www.cultura.gob.ar/media/uploads/15366666053_2be95c1db8_c.jpg",
-      },
-      {
-        title: "Plaza Italia",
-        urlImage: "https://upload.wikimedia.org/wikipedia/commons/a/af/Plaza_Italia_in_Buenos_Aires.jpg",
-      },
-      {
-        title: "Malba",
-        urlImage: "https://universes.art/fileadmin/user_upload/Specials/MALBA/malba-javier-agustin-rojas-2000-850.jpg",
-      },
-      {
-        title: "Barrio de San Telmo",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/san_telmo_general_calle_1200.jpg",
-      },
-      {
-        title: "Catedral Metropolitana",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/catedral-metropolitana-2021-fachada-1500x610.jpg",
-      },
-      {
-        title: "Parque de la Memoria",
-        urlImage: "https://buenosaires.gob.ar/sites/default/files/styles/full_width/public/media/image/2014/07/09/7a0458780942124841c960eac7da1f8abb9b29e7.jpg?itok=EVDz3Fmt",
-      },
-      {
-        title: "Planetario Galileo Galilei",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/planetario_noche_1200_1.jpg",
-      },
-      {
-        title: "Plaza Serrano",
-        urlImage: "https://ba-h.com.ar/wp-content/uploads/2021/05/plaza-serrano-palermo-feria.jpg",
-      },
-      {
-        title: "Barrio La Boca",
-        urlImage: "https://buenosaires.gob.ar/sites/default/files/media/image/2018/08/23/0cb9ff5cc29168e9728208dfb7596bbd35f15ddc.jpg",
-      },
-      {
-        title: "Costanera Norte",
-        urlImage: "https://www.buenosairesfreewalks.com/spanish/wp-content/uploads/2021/05/costanera-norte-1.jpg",
-      },
-      {
-        title: "Mercado de San Telmo",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/mercado_san_telmo1500x610_fachada.jpg",
-      },
-      {
-        title: "Museo Evita",
-        urlImage: "https://www.museoevitaresto.com.ar/wp-content/uploads/2019/04/1-Fachada-Museo.jpg",
-      },
-      {
-        title: "Barrio Palermo",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/planetario_agua_1200_2.jpg",
-      },
-      {
-        title: "Boca Juniors Stadium",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/estadio_boca_juniors_1200_cancha_1.jpg",
-      },
-      {
-        title: "Parque Centenario",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/parque-centenario-2023-1500x610.jpg",
-      },
-      {
-        title: "Palacio de Aguas Corrientes",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/palacio_aguas_corrientes_1200_a_0.jpg",
-      },
-      {
-        title: "Avenida Corrientes",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/field/image/calle_corrientes_noche_obelisco_1500x610.jpg",
-      },
-      {
-        title: "Museo de Arte Moderno",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/fachada-mumoderno-1500x610.jpg",
-      },
-      {
-        title: "El Rosedal",
-        urlImage: "https://services.meteored.com/img/article/el-rosedal-de-palermo-el-pulmon-verde-de-buenos-aires-aire-libre-1702936797598_1280.jpg",
-      },
-      {
-        title: "Plaza de los Dos Congresos",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/monumento%20dos%20congresos_0.jpg",
-      },
-      {
-        title: "Templo Libertad",
-        urlImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGWIl73hfkr1LnMAzO-_TrP97haHTh8nrrwg&s",
-      },
-      {
-        title: "Cerro de la Gloria",
-        urlImage: "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/2d/01/98.jpg",
-      },
-      {
-        title: "El Tigre",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/field/image/agua_tigre1500x610.jpg",
-      },
-      {
-        title: "Parque Lezama",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/field/image/parque_lezama_1200.jpg",
-      },
-      {
-        title: "Feria de Mataderos",
-        urlImage: "https://nadienosinvito.com.ar/wp-content/uploads/2017/07/5639e8576718e.jpg",
-      },
-      {
-        title: "Barrio Villa Devoto",
-        urlImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Edificio_Habana_4134%2C_esquina_Lincoln.jpg/640px-Edificio_Habana_4134%2C_esquina_Lincoln.jpg",
-      },
-      {
-        title: "Centro Cultural Recoleta",
-        urlImage: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Centro_Cultural_Recoleta_2012.jpg",
-      },
-      {
-        title: "Plaza de la República",
-        urlImage: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/62/03/a2/getlstd-property-photo.jpg?w=1200&h=1200&s=1",
-      },
-      {
-        title: "Jardín Japonés",
-        urlImage: "https://images.pagina12.com.ar/styles/focal_3_2_960x640/public/2023-07/752275-jard-c3-adn-20japon-c3-a9s.jpg?h=b3660f0d&itok=qe7Uifv_",
-      },
-      {
-        title: "Barrio Belgrano",
-        urlImage: "https://turismo.buenosaires.gob.ar/sites/turismo/files/estatua_belgrano_1200.jpg",
-      },
-      {
-        title: "Centro Cultural Kirchner",
-        urlImage: "https://periodico365.com/wp-content/uploads/2022/02/centro_cultural_kirchner.jpg",
-      },
-      {
-        title: "Barrio de Caballito",
-        urlImage: "https://upload.wikimedia.org/wikipedia/commons/9/99/Buenos_Aires_-_Caballito_-_Plazoleta_Cris%C3%B3logo_Larralde_2.jpg",
-      },
-      {
-        title: "Plaza Vicente López",
-        urlImage: "https://buenosaires.gob.ar/sites/default/files/styles/full_width/public/2023-03/VICENTE%20L%20Y%20PLANES%203_0.JPG?itok=F31Txesu",
-      },
+  
     ];
   
   
